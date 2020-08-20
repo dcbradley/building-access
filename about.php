5bb8fc39298eeca330686e68f7049443f6fae8c4
Dan Bradley <dan@physics.wisc.edu>
2020-08-20 17:29:38 -0500

    Allow the About page to be disabled.

--- about.php
+++ about.php
@@ -1,7 +1,9 @@
 <?php
 
-addUserMenuEntry( new MenuEntry('about','About','?s=about') );
-addPageHandler( new PageHandler('about','showAboutPage','container') );
+if( ABOUT_PAGE ) {
+  addUserMenuEntry( new MenuEntry('about','About','?s=about') );
+  addPageHandler( new PageHandler('about','showAboutPage','container') );
+}
 
 function showAboutPage() {
   echo ABOUT_PAGE;
